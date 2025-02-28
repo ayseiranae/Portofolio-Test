8a10559315e86f9a0baf4f306a9868b9bbbd4b3f
Ayseira Hidane <riesyanadiha@gmail.com>
2025-03-01 01:07:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,24 @@ function updateSlide() {
 }
 
 setInterval(nextSlide, 3000);
+
+const cursor = document.querySelector(".custom-cursor");
+document.addEventListener("mousemove", (e) => {
+  cursor.style.left = `${e.clientX}px`;
+  cursor.style.top = `${e.clientY}px`;
+});
+
+// animasi scroll
+document.addEventListener("scroll", () => {
+  const card = document.querySelector(".card");
+  const cardPosition = card.getBoundingClientRect().top;
+  const screenHeight = window.innerHeight;
+
+  if (cardPosition < screenHeight - 100) {
+    card.classList.add("visible");
+    card.classList.remove("hidden");
+  } else {
+    card.classList.add("hidden");
+    card.classList.remove("visible");
+  }
+});
